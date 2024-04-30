bb718bc68ab491d8b2841929872f4d86b6bb1804
Ngurah Deva <wirandanaputra@gmail.com>
2024-04-30 16:26:27 +0700

    finish project

--- Pic-a-Pal/ContentView.swift
+++ Pic-a-Pal/ContentView.swift
@@ -8,12 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var names : [String] = []
+    @State private var nameToAdd = ""
+    @State private var pickedName = ""
+    @State private var shouldRemovePickedName = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            VStack{
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                Text("Pick a Pal")
+            }
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .bold()
+            
+            Text(pickedName.isEmpty ? "  " : pickedName)
+                .font(.title2)
+                .bold()
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            List{
+                ForEach(names, id: \.self){
+                    name in Text(name)
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            
+            TextField("Add Name", text: $nameToAdd)
+                .autocorrectionDisabled()
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                }
+            
+            Toggle("Remove Picked Name", isOn: $shouldRemovePickedName)
+            
+            Button{
+                if let randomName = names.randomElement(){
+                    pickedName = randomName
+                    
+                    if shouldRemovePickedName {
+                        names.removeAll { name in
+                            return (name == randomName)
+                        }
+                    }
+                } else{
+                    pickedName = ""
+                }
+            } label: {
+                Text("Picked Random Name")
+                    .padding(.vertical, 12)
+                    .padding(.horizontal, 8)
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
         }
         .padding()
     }
